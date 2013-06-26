a018df1dccdda36dbbfc8a3073d51537ac0f52b2
unknown <mpifalo@.corp.epnet.com>
2013-06-26 15:49:20 -0400

    added validation module

--- framework.js
+++ framework.js
@@ -1,9 +1,308 @@
 /**
  * @fileoverview Contains all of the global functionality used in conjunction with the framework
  * @license Copyright EBSCO Industries, Inc.
- * @author Mike Pifalo
- * @author Jeff Ayer
+ * @author EBSCO Marketing Web Development
+ * 
+ * 
+ * Conventions:
+ * GLOBAL_VARIABLE 	:: 	any variable or method that is global should be written in all caps, underscore
+ * localVariable 	:: 	any variable or method that is local scope should be written in camelcase
+ * _privateMethod 	:: 	any variable or method that is private should be underscore prefixed
+ * 
+ * TODO: some task 	:: 	any future work should be noted with "TODO:" 
+ * 
+ * 
  */
+
+
+
+/*
+ * GLOBAL module VALIDATE
+ * 
+ * Uses loose augentention; any other js file can add to this global object:
+ * var VALIDATE = (function () {
+ * 		var my = {};
+ * 		// your code here
+ * 		my.yourPublicMethod = function () { }
+ * 		my.yourPublicProperty =  some value
+ * 
+ * 		return my;
+ * }(VALIDATE || {}));
+ * 
+ * Public methods:
+ * 		VALIDATE.email(element) - validate element's value using email rules (#@#.)
+ * 			returns 1 if error
+ * 		VALIDATE.radio(element) - validate element's value using radio rules
+ * 			returns 1 if error
+ * 		VALIDATE.text(elements) - validate element's value using length (any value of length 1 or greater)
+ * 			returns 1 if error
+ * 		VALIDATE.testfields(elements) - validates either specific passed in element(s) or elements with class 'required'
+ * 			returns 1 if error
+ * 
+ * 		VALIDATE.checkSubmitState() - checks form for inputs with class 'error' and sets submit btn to disabled if found
+ * 			returns undefined
+ * 
+ * 		VALIDATE.moveLabel(element) - assumes element is input;
+ * 										sets class of label for element to 'hasValue';
+ * 										sets class of multiline label for element to 'active';
+ * 										sets class of prefix label for element to 'active'
+ * 		VALIDATE.removeLabel(element) - removes classes set by VALIDATE.moveLabel(element)
+ * 		VALIDATE.checkValue(element) - assumes element is input;
+ * 										if element value is not empty, calls VALIDATE.moveLabel(element), otherwise calls VALIDATE.removeLabel(element)
+ * 
+ * 		VALIDATE.init() - binds VALIDATE.testfields() to click event of any submit element;
+ * 							binds VALIDATE.testfields(element) to blur event of every input with class 'required'
+ * 							binds VALIDATE.moveLabel(element) to click, focus event of every input with class 'required';
+ * 							binds VALIDATE.checkValue(element) to blur event of every input with class 'required'
+ * 
+ * (technically public but really just used privately) TODO: either make private or move to new global module
+ * 		VALIDATE.setClass(element, class string) - adds a given class name to an element
+ * 		VALIDATE.removeClass(element, class string) - removes a given class name from an element
+ */
+var VALIDATE = (function () {
+    var my = {};
+    // private variables
+    // var privatevarname = ...
+
+
+	// private methods
+	// function privateMethod() { ... }
+	
+	// below are not technically private...yet
+	// set the passed in element to the provided class
+	my.setClass = function(aElem, aClass) {
+		try {
+			var aElem = $(aElem)[0];	// getting the dom obj from the jQ obj
+			// we only need to set the error class if it's not already on the field...
+			if ( (aElem.className).indexOf(aClass) === -1 ) {
+				var classList = (aElem.className).split(' ');
+				classList.push(aClass);
+				aElem.className = classList.join(' ');
+			}
+		} catch(e) {
+			// console.log(e);
+		}
+	};
+	
+	// remove  the provided class from the passed in element 
+	my.removeClass = function(aElem, aClass) {
+		try {
+			var aElem = aElem[0];	// getting the dom obj from the jQ obj
+			aElem.className = (aElem.className).replace(aClass, '');
+		} catch (e) {
+			// console.log(e);
+		}
+	};
+	
+	
+	// public properties
+	// my.propertyname
+	
+	
+	// public methods
+	// my.methodname
+	
+	// checks element for valid email value and adds/removes error class (invalid) on element
+	// returns 1 if error
+	my.email = function (aElem) {
+	    //regex source http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
+	    //var element = $(email);
+	    var re = /\S+@\S+\.\S+/,
+	    	element = $(aElem),
+	    	thisError = 0;
+	    
+	    if ( !(re.test(element.val())) ) {
+	    	thisError = 1;
+	    }
+	    return thisError;
+	};
+	
+	// returns 1 if error
+	my.radio = function (aElem) {
+        var checkName = $(aElem).attr('name'),
+        	elementNames = [],
+        	thisError = 0;
+        
+        elementNames.push(checkName);
+
+        for (i = 0; i < elementNames.length; i++) {
+            var thisId = $(this).attr('id'),
+                isChecked = $('input[name="' + elementNames[i] + '"]').is(':checked');
+            
+            if (!isChecked) {
+                $('label[for=' + thisId + ']').addClass('error');
+                thisError = 1;
+                //return errors;
+            } else {
+                $('label[for=' + thisId + ']').removeClass('error');
+            }
+        }
+        return thisError;
+	};
+	
+	my.text = function (aElem) {
+		var elementVal = aElem.val(),
+			thisError = 0;
+        if (elementVal === '') {
+            thisError = 1;
+        }
+        return thisError;
+	};
+	
+	// returns 1 if error
+	my.testfields = function (aField) {
+	    //check to see if any required fields
+	    //are unchecked, blank, or have well
+	    //formatted email addresses
+	    var required,
+	    	submit = $('.form-behavior input[type="submit"], .form-behavior button[type="submit"]'),
+	        elementNames = [],
+	        error = 0;
+	    
+	    // if no element passed in, assume check all .required fields
+	    // otherwise check just the one
+		if (aField === undefined ) {
+			required = $('.required:visible');
+		} else {
+			required = $(aField);
+		}
+
+	    required.each(function(){
+	        
+	        var $that = $(this),
+	            elementType = $that.attr('type'),
+	            thisError = 0;
+	            
+	       // check field for error based on input type
+	        if (elementType === 'email') {
+		        //email validation
+	            if ( VALIDATE.email($that) !== 0 ) {
+	            	thisError = 1;
+	            }
+	        } else if (elementType === 'radio') {   
+	        	//radio button validation
+	        	if ( VALIDATE.radio($that) !== 0 ) {
+	            	thisError = 1;
+	            };
+	        } else {
+	        	// text inputs...
+	        	if ( VALIDATE.text($that) !== 0 ) {
+	        		thisError = 1;
+	        	}
+	        }
+	        
+			// set or remove error classes
+	        if ( thisError === 1 ) {
+	        	VALIDATE.setClass($that,'error');
+	        	error = thisError;
+	        } else {
+	        	VALIDATE.removeClass($that,'error');
+	        }
+	        
+	    });
+	    
+	    VALIDATE.checkSubmitState();
+	    
+		return error;
+	};
+	
+	// checks if form has errors and sets submit btn class
+	my.checkSubmitState = function() {
+		var submit = $('.form-behavior input[type="submit"], .form-behavior button[type="submit"]'),
+			hasErrors = $('.error').length;
+
+		if (hasErrors !== 0) {
+			//console.log('form has errors');
+			VALIDATE.setClass(submit,'disabled');
+			submit.attr('disabled','disabled');
+		} else {
+			//console.log('form OK');
+			VALIDATE.removeClass(submit,'disabled');
+			submit.removeAttr('disabled');
+		}
+	};
+	
+	my.moveLabel = function(inputField) {
+		var thisLabel = $('label[for="' + inputField.attr('id') + '"]');
+
+		VALIDATE.setClass( thisLabel, 'hasValue' );
+
+		// if input is child of multiline classed parent, add active to the input
+		if ( $(inputField).parent().hasClass('multiline') ) {
+			VALIDATE.setClass( $(inputField).parent('.multiline'), 'active' );
+		}
+		// if input field has a prefix (like 'http://') then add active class to the prefix element
+		if ( inputField.hasClass('hasPrefix') ) {
+			VALIDATE.setClass( inputField.prev('.field-prefix'), 'active' );
+		}
+	};
+
+	my.removeLabel = function(inputField) {
+		var thisLabel = $('label[for="' + inputField.attr('id') + '"]');
+		
+		VALIDATE.removeClass( thisLabel, 'hasValue' );
+		
+		if ( $(inputField).parent().hasClass('multiline') ) {
+			VALIDATE.removeClass( $(inputField).parent('.multiline'), 'active' );
+		}
+		// if input field has a prefix (like 'http://') then add active class to the prefix element
+		if ( inputField.hasClass('hasPrefix') ) {
+			VALIDATE.removeClass( inputField.prev('.field-prefix'), 'active' );
+		}
+	};
+	
+	my.checkValue = function(inputField) {
+		// check value of input field, and if not empty move label out
+		// otherwise move label in
+		var thisInputVal = inputField.val();
+		if ( thisInputVal !== '' ) {
+			VALIDATE.moveLabel(inputField);
+		} else {
+			VALIDATE.removeLabel(inputField);
+		}
+	};
+	
+	
+	
+	// binds validation to click event of submit buttons
+	my.init = function () {
+		var submit = $('.form-behavior input[type="submit"], .form-behavior button[type="submit"]'),
+			required = $('.form-behavior').find('.required:visible'),
+			thisError = 0;
+		
+		submit.on('click', function() {
+			thisError = VALIDATE.testfields();
+			if (thisError !== 0) {
+				// form has errors
+				return false;
+			} else {
+				// form OK
+				return true;
+			}
+		});
+		
+		required.each(function(){
+			$(this).on({
+				click : function() {
+					VALIDATE.moveLabel($(this));
+					//moveLabel($(this));
+				},
+				focus : function() {
+					VALIDATE.moveLabel($(this));
+					//moveLabel($(this));
+				},
+				blur : function() {
+					checkValue($(this));
+					VALIDATE.testfields($(this));
+				}
+			});
+		});
+	};
+	
+	// return public methods
+	return my;
+}(VALIDATE || {}));
+
 
 
 /**
@@ -278,6 +577,9 @@ jQuery(function($) {
 						$(window).resize(function() {
 							setupImageMQ(that);
 						});
+						break;
+					case "form-behavior":
+						VALIDATE.init();
 						break;
 				}
 			}
